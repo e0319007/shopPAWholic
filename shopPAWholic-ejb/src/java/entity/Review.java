1d593291d7475efea36b7c02d262a4aa2b873711
Shi Zhan <Shi Zhan@LAPTOP-4NC54K22>
2020-03-01 13:38:32 +0800

    add Review Entity

---
+++ shopPAWholic-ejb/src/java/entity/Review.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Shi Zhan
+ */
+@Entity
+public class Review implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
+    private Long reviewId;
+    private String description;
+    private int rating;
+    private Date date;
+    private List<String> reviewPictures;
+
+    public Long getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(Long reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (reviewId != null ? reviewId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the reviewId fields are not set
+        if (!(object instanceof Review)) {
+            return false;
+        }
+        Review other = (Review) object;
+        if ((this.reviewId == null && other.reviewId != null) || (this.reviewId != null && !this.reviewId.equals(other.reviewId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.Review[ id=" + reviewId + " ]";
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public List<String> getReviewPictures() {
+        return reviewPictures;
+    }
+
+    public void setReviewPictures(List<String> reviewPictures) {
+        this.reviewPictures = reviewPictures;
+    }
+    
+}
