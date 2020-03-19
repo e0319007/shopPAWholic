5fc0051ae3823ed722ea0f1f7d039130d74bf5c9
Shi Zhan <Shi Zhan@LAPTOP-4NC54K22>
2020-03-19 16:26:39 +0800

    add CRUD to forumpost managed bean

--- shopPAWholic-war/src/java/jsf/managedBean/ForumPostManagedBean.java
+++ shopPAWholic-war/src/java/jsf/managedBean/ForumPostManagedBean.java
@@ -14,12 +14,17 @@ import java.util.List;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 import javax.annotation.PostConstruct;
 import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import util.exception.CreateNewForumPostException;
+import util.exception.ForumPostNotFoundException;
+import util.exception.ForumTitleExistsException;
+import util.exception.InputDataValidationException;
 
 /**
  *
@@ -32,33 +37,137 @@ public class ForumPostManagedBean implements Serializable{
     @EJB(name = "ForumPostSessionBeanLocal")
     private ForumPostSessionBeanLocal forumPostSessionBeanLocal;
 
-    /**
-     * Add new forum post
-     */
-    @Size(min = 10, message = "Forum post must be more than 10 characters")
+    //Add new forum post
     private String content;
-    @NotNull
     private Date date;
-    @NotNull
     private int thumbsUpCount;
-    @NotNull
-    @Size(min = 5, message = "Title must be more than 5 characters")
     private String title;
-    @NotNull
     private boolean deleted;
-    @ManyToOne(optional = true)
     private Customer customer;
-    @OneToMany
-    private List<Comment> comments;
     
-    List<ForumPost> forumPosts;
+    //Retrieve all forum post
+    private List<ForumPost> forumPosts;
+    
+    //update forum post
+    private ForumPost forumPostToUpdate;
     
     public ForumPostManagedBean() {
     }
     
     @PostConstruct
     public void PostConstruct() {
-        
+       forumPosts = forumPostSessionBeanLocal.retrieveAllForumPost();
     }
     
+    public void createNewForumPost(ActionEvent event) {
+        ForumPost forumPost = new ForumPost(content, date, title);
+        try {
+            forumPostSessionBeanLocal.createNewForumPost(forumPost, customer.getUserId());
+             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "New forum post created successfully! (Id: " + forumPost.getForumId()+ ")", null));
+        } catch (ForumTitleExistsException ex) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Forum post with title " + forumPost.getTitle() + "exists: " + ex.getMessage(), null));
+        } catch(InputDataValidationException | CreateNewForumPostException ex) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "An error has occurred while creating forum post: " + ex.getMessage(), null));
+        }
+    }
+    
+    public void updateForumPost(ActionEvent event) {
+        try {
+            forumPostSessionBeanLocal.updateForumPost(forumPostToUpdate);
+        } catch (InputDataValidationException ex) {
+             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "An error has occurred while editing forum post: " + ex.getMessage(), null));
+        } catch (ForumTitleExistsException ex) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Forum post with title " + forumPostToUpdate.getTitle() + "exists: " + ex.getMessage(), null));
+        }
+    }
+    
+    public void deleteForumPost(ActionEvent event) {
+        ForumPost forumPostToDelete = (ForumPost) event.getComponent().getAttributes().get("forumPostToDelete");
+        try {
+            forumPostSessionBeanLocal.deleteForumPost(forumPostToDelete.getForumId());
+        } catch (ForumPostNotFoundException ex) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Forum post with given ID is not found", null));
+        }
+    }
+    
+    public void thumbsUp(ActionEvent event) {
+        ForumPost forumPostToLike = (ForumPost) event.getComponent().getAttributes().get("forumPostToLike");
+        try {
+            forumPostSessionBeanLocal.thumbsUp(forumPostToLike.getForumId());
+        } catch (ForumPostNotFoundException ex) {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "Forum post with given ID is not found", null));
+        }
+    }
+
+    public ForumPostSessionBeanLocal getForumPostSessionBeanLocal() {
+        return forumPostSessionBeanLocal;
+    }
+
+    public void setForumPostSessionBeanLocal(ForumPostSessionBeanLocal forumPostSessionBeanLocal) {
+        this.forumPostSessionBeanLocal = forumPostSessionBeanLocal;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getThumbsUpCount() {
+        return thumbsUpCount;
+    }
+
+    public void setThumbsUpCount(int thumbsUpCount) {
+        this.thumbsUpCount = thumbsUpCount;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        this.deleted = deleted;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public List<ForumPost> getForumPosts() {
+        return forumPosts;
+    }
+
+    public void setForumPosts(List<ForumPost> forumPosts) {
+        this.forumPosts = forumPosts;
+    }
+
+    public ForumPost getForumPostToUpdate() {
+        return forumPostToUpdate;
+    }
+
+    public void setForumPostToUpdate(ForumPost forumPostToUpdate) {
+        this.forumPostToUpdate = forumPostToUpdate;
+    }
 }
