36484687daeb9c37ebe5e95d61d94f6bdadf1541
joannang <50539345+joannang@users.noreply.github.com>
2020-04-01 11:46:54 +0800

    application config

--- shopPAWholic-rws/src/java/ws/restful/ApplicationConfig.java
+++ shopPAWholic-rws/src/java/ws/restful/ApplicationConfig.java
@@ -27,6 +27,7 @@ public class ApplicationConfig extends Application {
         resources.add(ws.restful.CartResource.class);
         resources.add(ws.restful.CategoryResource.class);
         resources.add(ws.restful.ListingResource.class);
+        resources.add(ws.restful.OrderEntityResource.class);
         resources.add(ws.restful.ReviewResource.class);
         resources.add(ws.restful.TagResource.class);
         resources.add(ws.restful.UserResource.class);
