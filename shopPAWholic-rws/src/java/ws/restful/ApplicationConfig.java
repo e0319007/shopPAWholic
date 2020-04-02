9e3dbbfb0adfc13bed1e753faa7b39294cdee51b
EileenLeong <EileenLeong@172.25.107.218>
2020-04-02 10:02:35 +0800

    application

--- shopPAWholic-rws/src/java/ws/restful/ApplicationConfig.java
+++ shopPAWholic-rws/src/java/ws/restful/ApplicationConfig.java
@@ -28,13 +28,6 @@ public class ApplicationConfig extends Application {
         resources.add(ws.restful.CategoryResource.class);
         resources.add(ws.restful.DeliveryDetailsResource.class);
         resources.add(ws.restful.ListingResource.class);
-<<<<<<< HEAD
-
-
         resources.add(ws.restful.OrderEntityResource.class);
-
-=======
-        resources.add(ws.restful.OrderEntityResource.class);
->>>>>>> origin/shizhan
     }
 }
