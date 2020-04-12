6e0653bac591876a5830bd8cf969bfd6edb35302
Shi Zhan <Shi Zhan@LAPTOP-4NC54K22>
2020-04-12 16:03:29 +0800

    add app routing module

--- shopPAWholicIonic/src/app/app-routing.module.ts
+++ shopPAWholicIonic/src/app/app-routing.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from './guards/auth.guard';
+
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'home',
     pathMatch: 'full'
-  },
-  {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
-  },
+  },  
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  }
+  },  
+  { path: 'viewAllListings', loadChildren: './view-all-listings/view-all-listings.module#ViewAllListingsPageModule', canActivate: [AuthGuard] },
+  { path: 'createNewListing', loadChildren: './create-new-listing/create-new-listing.module#CreateNewListingPageModule', canActivate: [AuthGuard] },
+  { path: 'viewOrders', loadChildren: './view-orders/view-orders.module#ViewOrdersPageModule', canActivate: [AuthGuard] },
+  { path: 'viewShoppingCart', loadChildren: './view-shopping-cart/view-shopping-cart.module#ViewShoppingCartPageModule', canActivate: [AuthGuard] },
+  { path: 'viewAdvertisements', loadChildren: './view-advertisements/view-advertisements.module#ViewAdvertisementsPageModule', canActivate: [AuthGuard] },
+  { path: 'viewReviews', loadChildren: './view-reviews/view-reviews.module#ViewReviewsPageModule', canActivate: [AuthGuard] }
 ];
 
 @NgModule({
