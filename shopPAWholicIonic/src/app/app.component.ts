45dd2818973898d3badd7fec5a95100e229c2f7f
Shi Zhan <Shi Zhan@LAPTOP-4NC54K22>
2020-04-12 15:58:10 +0800

    add proxy config file

--- shopPAWholicIonic/src/app/app.component.ts
+++ shopPAWholicIonic/src/app/app.component.ts
@@ -13,34 +13,34 @@ export class AppComponent implements OnInit {
   public selectedIndex = 0;
   public appPages = [
     {
-      title: 'Inbox',
-      url: '/folder/Inbox',
-      icon: 'mail'
+      title: 'View All Listings',
+      url: '/viewAllListings',
+      icon: 'arrow-forward'
     },
     {
-      title: 'Outbox',
-      url: '/folder/Outbox',
-      icon: 'paper-plane'
+      title: 'Create New Listing',
+      url: '/createNewListing',
+      icon: ''
+    }
+    {
+      title: 'View Orders',
+      url: '/viewAllOrders',
+      icon: ''
     },
     {
-      title: 'Favorites',
-      url: '/folder/Favorites',
-      icon: 'heart'
+      title: 'Shopping Cart',
+      url: '/viewShoppingCart',
+      icon: ''
     },
     {
-      title: 'Archived',
-      url: '/folder/Archived',
-      icon: 'archive'
+      title: 'Advertisements',
+      url: '/viewAdvertisements',
+      icon: ''
     },
     {
-      title: 'Trash',
-      url: '/folder/Trash',
-      icon: 'trash'
-    },
-    {
-      title: 'Spam',
-      url: '/folder/Spam',
-      icon: 'warning'
+      title: 'View Reviews',
+      url: '/viewReviews',
+      icon: ''
     }
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
