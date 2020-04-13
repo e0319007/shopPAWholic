f37910c4998ae1433e00da90d548fc218a77cd83
Shi Zhan <Shi Zhan@LAPTOP-4NC54K22>
2020-04-13 14:44:33 +0800

    add cart.ts

--- shopPAWholicIonic/src/app/billing-detail.ts
+++ shopPAWholicIonic/src/app/billing-detail.ts
@@ -1,2 +1,21 @@
+
+import { Seller } from './seller';
+import { Customer } from './customer';
+import { Advertisement } from './advertisement';
+import { OrderEntity } from './order-entity';
+
 export class BillingDetail {
+    billingDetailId: number;
+    creditCardDetail: string;
+    billingDate: string;
+    customer: Customer;
+    seller: Seller;
+    advertisement: Advertisement;
+    order: OrderEntity
+    
+    constructor(billingDetailId?: number, creditCardDetail?: string,
+                billingDate ?: string, customer?: Customer, seller?: Seller,
+                advertisement?: Advertisement, order?: OrderEntity) {
+
+    }
 }
