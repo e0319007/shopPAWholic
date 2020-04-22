ccbd37447cde246c42f3f4d952f97c921dae2e2a
EileenLeong <EileenLeong@172.25.107.218>
2020-04-22 19:11:43 +0800

    added pics folder

--- shopPAWholic-war/src/java/jsf/managedBean/ImageGalleriaManagedBean.java
+++ shopPAWholic-war/src/java/jsf/managedBean/ImageGalleriaManagedBean.java
@@ -20,7 +20,7 @@ public class ImageGalleriaManagedBean {
     @PostConstruct
     public void init() {
         images = new ArrayList<String>();
-        for (int i = 1; i <= 3; i++) {
+        for (int i = 1; i <= 4; i++) {
             images.add("petAdvertisement" + i + ".jpg");
         }
     }
