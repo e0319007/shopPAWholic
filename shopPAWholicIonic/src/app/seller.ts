1eaa1a28c65383a9d4a6b118eed52b99423818cb
Shi Zhan <Shi Zhan@LAPTOP-4NC54K22>
2020-04-28 09:46:00 +0800

    nb project

--- shopPAWholicIonic/src/app/seller.ts
+++ shopPAWholicIonic/src/app/seller.ts
@@ -1,17 +1,9 @@
 import { User } from './user';
 
-<<<<<<< HEAD
-
-=======
->>>>>>> master
 export class Seller extends User{
 
     constructor(userId?: number, name?: string, email?: string, contactNumber?: string, password?: string, 
         accCreatedDate?: Date) {
         super(userId, name, email, contactNumber, password, accCreatedDate);
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> master
 }
