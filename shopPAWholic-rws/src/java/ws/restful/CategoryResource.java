913ecc53638003aa199278d64ad6ad10783307d8
EileenLeong <EileenLeong@172.25.107.218>
2020-04-28 20:11:47 +0800

    resolved conflict w joanna

--- shopPAWholic-rws/src/java/ws/restful/CategoryResource.java
+++ shopPAWholic-rws/src/java/ws/restful/CategoryResource.java
@@ -4,109 +4,82 @@
  * and open the template in the editor.
  */
 package ws.restful;
-import ejb.session.stateless.ListingSessionBeanLocal;
+
+import ejb.session.stateless.CategorySessionBeanLocal;
+import ejb.session.stateless.UserSessionBean;
 import ejb.session.stateless.UserSessionBeanLocal;
-import entity.Listing;
-import entity.OrderEntity;
-import entity.Review;
+import entity.Category;
 import entity.Seller;
-import entity.Tag;
-import entity.User;
 import java.util.List;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Produces;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
-import util.exception.CreateNewListingException;
+import util.exception.AdvertisementNotFoundException;
+import util.exception.CreateNewAdvertisementException;
+import util.exception.InputDataValidationException;
 import util.exception.InvalidLoginCredentialException;
-import util.exception.ListingNotFoundException;
-import util.exception.UpdateListingException;
-import ws.datamodel.ListingCreateReq;
 import ws.datamodel.ErrorRsp;
-import ws.datamodel.ListingCreateNewRsp;
-import ws.datamodel.ListingRetrieveDetailRsp;
-import ws.datamodel.ListingUpdateReq;
-import ws.datamodel.ListingsRetrieveAllRsp;
-import java.util.Date;
+import ws.datamodel.CategoriesRetrieveAllRsp;
 
 /**
  *
  * @author shizhan
  */
-
-@Path("Listing")
-public class ListingResource {    
+@Path("Category")
+public class CategoryResource
+{    
     @Context
     private UriInfo context;
     
     private final SessionBeanLookup sessionBeanLookup;
     
     private final UserSessionBeanLocal userSessionBeanLocal;
-    private final ListingSessionBeanLocal listingSessionBeanLocal;
-    
-    
-    
-    public ListingResource() 
-    {
-        sessionBeanLookup = new SessionBeanLookup();
+    private final CategorySessionBeanLocal categorySessionBeanLocal;
+
+    public CategoryResource() {
         
+        sessionBeanLookup = new SessionBeanLookup();
         userSessionBeanLocal = sessionBeanLookup.lookupUserSessionBeanLocal();
-        listingSessionBeanLocal = sessionBeanLookup.lookupListingSessionBeanLocal();
+        categorySessionBeanLocal = sessionBeanLookup.lookupCategorySessionBeanLocal();
     }
     
-    public Seller getSellerInstance(String email, String password, String methodName) throws InvalidLoginCredentialException {
-        User user =  userSessionBeanLocal.userLogin(email, password);
-        System.out.println("********** ListingResource." + methodName + "(): User " + user.getEmail()+ " login remotely via web service");
-        if(user instanceof Seller) return (Seller) user;
-        else throw new InvalidLoginCredentialException();
-    }
-    
-    @Path("retrieveAllListings")
+    @Path("retrieveAllCategories")
     @GET
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response retrieveAllListings() {
-        try {
-            List<Listing> listings = listingSessionBeanLocal.retrieveAllListings();
-            for(Listing l:listings) {
-                if(l.getCategory() != null) {
-                
-                    l.getCategory().getListings().clear();
-              
-                }
-                for(Tag t:l.getTags()) {
-                    t.getListings().clear();
-                }
-                l.getSeller().getListings().clear();
-                l.getSeller().getAdvertisements().clear();
-                l.getSeller().getBillingDetails().clear();
-                l.getSeller().getEvents().clear();
-                l.getSeller().getOrders().clear();
-                
-                for (Review r: l.getReviews()) {
-                    r.setListing(null);
-                    r.getCustomer().setBillingDetails(null);
-                    r.getCustomer().setCart(null);
-                    r.getCustomer().setComments(null);
-                    r.getCustomer().setForumPosts(null);
-                    r.getCustomer().setReviews(null);
-                    r.getCustomer().setOrders(null);
-                }
-                l.getOrders().clear();
-                
-                
+    public Response retrieveAllCategories(@QueryParam("email") String email, 
+                                        @QueryParam("password") String password)
+    {
+        try
+        {
+            Seller seller = (Seller) userSessionBeanLocal.userLogin(email, password);
+            System.out.println("********** CategoryResource.retrieveAllCategories(): seller " + seller.getEmail()+ " login remotely via web service");
+
+            List<Category> categoryEntities = categorySessionBeanLocal.retrieveAllCategories();
+            
+            for(Category category:categoryEntities) {
+                category.getListings().clear();
             }
-            return Response.status(Status.OK).entity(new ListingsRetrieveAllRsp(listings)).build();
+            return Response.status(Status.OK).entity(new CategoriesRetrieveAllRsp(categoryEntities)).build();
+        }
+        catch(InvalidLoginCredentialException ex) {
+            ErrorRsp errorRsp = new ErrorRsp(ex.getMessage());
+            
+            return Response.status(Status.UNAUTHORIZED).entity(errorRsp).build();
         }
         catch(Exception ex) {
             ErrorRsp errorRsp = new ErrorRsp(ex.getMessage());
@@ -114,140 +87,29 @@ public class ListingResource {
         }
     }
     
-    
-    
-    @Path("retrieveListing/{listingId}")
+    @Path("retrieveAllLeafCategories")
     @GET
     @Consumes(MediaType.TEXT_PLAIN)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response retrieveListing(@PathParam("listingId") Long listingId) {
-        System.out.println("RetrieveListing by id is called");
-        try {
-            Listing listing = listingSessionBeanLocal.retrieveListingByListingId(listingId);
-            
-            if(listing.getCategory() != null) {
-                
-                listing.getCategory().getListings().clear();
-            
-            }
+    public Response retrieveAllLeafCategories(@QueryParam("email") String email, 
+                                        @QueryParam("password") String password) {
+        try
+        {
+            Seller seller = (Seller) userSessionBeanLocal.userLogin(email, password);
+            System.out.println("********** CategoryResource.retrieveAllLeafCategories(): seller " + seller.getEmail()+ " login remotely via web service");
 
-            for(Tag t:listing.getTags()) {
-                t.getListings().clear();
+            List<Category> categoryEntities = categorySessionBeanLocal.retrieveAllCategories();
+            
+            for(Category category:categoryEntities) {
+                category.getListings().clear();
+                System.out.println(category.getDescription());
             }
-            listing.getSeller().getListings().clear();
-            listing.getSeller().getAdvertisements().clear();
-            listing.getSeller().getBillingDetails().clear();
-            listing.getSeller().getEvents().clear();
-            listing.getSeller().getOrders().clear();
-
-            for (Review r: listing.getReviews()) {
-                r.setListing(null);
-                r.getCustomer().setBillingDetails(null);
-                r.getCustomer().setCart(null);
-                r.getCustomer().setComments(null);
-                r.getCustomer().setForumPosts(null);
-                r.getCustomer().setReviews(null);
-                r.getCustomer().setOrders(null);
-            }
-            listing.getOrders().clear();
-                
-            return Response.status(Status.OK).entity(new ListingRetrieveDetailRsp(listing)).build(); 
+            return Response.status(Status.OK).entity(new CategoriesRetrieveAllRsp(categoryEntities)).build();
         }
-        catch(ListingNotFoundException ex) {
+        catch(InvalidLoginCredentialException ex) {
             ErrorRsp errorRsp = new ErrorRsp(ex.getMessage());
-            return Response.status(Status.BAD_REQUEST).entity(errorRsp).build();
-        }
-        catch(Exception ex) {
-            ErrorRsp errorRsp = new ErrorRsp(ex.getMessage());   
-            return Response.status(Status.INTERNAL_SERVER_ERROR).entity(errorRsp).build();
-        }
-    }
-    
-    
-    
-    @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response createListing(ListingCreateReq createListingReq)
-    {
-        if(createListingReq != null) {
-            try {
-                Seller seller = getSellerInstance(createListingReq.getEmail(), createListingReq.getPassword(), "createListing");
-                createListingReq.getListing().setListDate(new Date());
-                Listing listing  = listingSessionBeanLocal.createNewListing(createListingReq.getListing(), createListingReq.getCategoryId(), createListingReq.getTagIds(), seller.getUserId());
-                
-                return Response.status(Response.Status.OK).entity(new ListingCreateNewRsp(listing.getListingId())).build();
-            }
-            catch(InvalidLoginCredentialException ex) {
-                ErrorRsp errorRsp = new ErrorRsp(ex.getMessage());
-                return Response.status(Status.UNAUTHORIZED).entity(errorRsp).build();
-            }
-            catch(CreateNewListingException ex) {
-                ErrorRsp errorRsp = new ErrorRsp(ex.getMessage());
-                return Response.status(Response.Status.BAD_REQUEST).entity(errorRsp).build();
-            }
-            catch(Exception ex) {
-                ErrorRsp errorRsp = new ErrorRsp(ex.getMessage());
-                return Response.status(Status.INTERNAL_SERVER_ERROR).entity(errorRsp).build();
-            }
-        }
-        else {
-            ErrorRsp errorRsp = new ErrorRsp("Invalid create new product request");
-            return Response.status(Response.Status.BAD_REQUEST).entity(errorRsp).build();
-        }
-    }
-    
-   
-    
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response updateListing(ListingUpdateReq listingUpdateReq)
-    {
-        if(listingUpdateReq != null) {
-            try {                
-                Seller seller = getSellerInstance(listingUpdateReq.getEmail(), listingUpdateReq.getPassword(), "updateListing");
-                
-                listingSessionBeanLocal.updateListing(listingUpdateReq.getListing(), listingUpdateReq.getCategoryId(), listingUpdateReq.getTagIds());
-                System.out.println("********** qty on hand : " + listingUpdateReq.getListing().getQuantityOnHand());
-                return Response.status(Response.Status.OK).build();
-            }
-            catch(InvalidLoginCredentialException ex) {
-                ErrorRsp errorRsp = new ErrorRsp(ex.getMessage());
-                return Response.status(Status.UNAUTHORIZED).entity(errorRsp).build();
-            }
-            catch(UpdateListingException ex) {
-                ErrorRsp errorRsp = new ErrorRsp(ex.getMessage());
-                return Response.status(Response.Status.BAD_REQUEST).entity(errorRsp).build();
-            }
-            catch(Exception ex) {
-                ErrorRsp errorRsp = new ErrorRsp(ex.getMessage());
-                return Response.status(Status.INTERNAL_SERVER_ERROR).entity(errorRsp).build();
-            }
-        }
-        else {
-            ErrorRsp errorRsp = new ErrorRsp("Invalid update product request");
-            return Response.status(Response.Status.BAD_REQUEST).entity(errorRsp).build();
-        }
-    }
-    
-    
-    
-    @Path("{listingId}")
-    @DELETE
-    @Consumes(MediaType.TEXT_PLAIN)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response deleteListing(@QueryParam("email") String email, 
-                                        @QueryParam("password") String password,
-                                        @PathParam("listingId") Long listingId) {
-        try {
-            Seller seller = getSellerInstance(email, password, "deleteListing");
-            listingSessionBeanLocal.deleteListing(listingId);
-            return Response.status(Status.OK).build();
-        }
-        catch(ListingNotFoundException ex) {
-            ErrorRsp errorRsp = new ErrorRsp(ex.getMessage());
-            return Response.status(Status.BAD_REQUEST).entity(errorRsp).build();
+            
+            return Response.status(Status.UNAUTHORIZED).entity(errorRsp).build();
         }
         catch(Exception ex) {
             ErrorRsp errorRsp = new ErrorRsp(ex.getMessage());
@@ -255,4 +117,3 @@ public class ListingResource {
         }
     }
 }
-
